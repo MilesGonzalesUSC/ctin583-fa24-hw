8450a60195c929448b2e4741f5f3e0c1ff1b11bf
Miles Gonzales <milesgon@usc.edu>
2024-09-25 20:02:06 -0700

    Finished Homework

--- hw05/CollisionHandler.cs
+++ hw05/CollisionHandler.cs
@@ -19,28 +19,74 @@ using UnityEngine.SearchService;
 
     Problem 5: TODO: 
         * When would you use a tuple over a struct?
+            ANSWER:
+            I would use a tuple over a struct if I need to return multiple values and not have a data intensive solution.
+            It is also a good idea to use tuples when the order of the items actually matters for your solution
         * How do we acces items in a tuple?
+            ANSWER:
+            tuples use indexing to access the data within. So if I needed to 
+            access an the first element of a tuple called Tup, I would type Tup[0].
+
         * Try visualizing your enum in the Unity Editor. How does it appear as?
+            ANSWER:
+            The unity editor would show your enum as a dropdown menu.
+            You should also be able to choose what values you want to include by selecting and deselecting them.
         
 ******************************************************************************************************
 */
 public class CollisionHandler : MonoBehaviour
 {
+
+    public enum Particles{
+        FireParticles = 1,
+        GoldRibbons = 2,
+
+    }
     [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private ParticleSystem enemyParticles;
     [SerializeField] private CollectibleItems collectibles; 
 
+    (string, int, int) NormTuple = ("Miles", 22, 2001);
+    var NormTuple = ("Miles", 22, 2001);
+
+    private void Start(){
+        Debug.log($"Name:{NormTuple.Item1}Age:{NormTuple.Item2}Year of Birth:{NormTuple.Item3}");
+    }
+
     private void OnCollisionEnter(Collision collision) {
         switch (collision.gameObject.tag) {
-            case "Enemy":
+            case 7:
                 Destroy(gameObject);
                 break;
 
-            case "Gem":
+            case 3:
                 meshRenderer.material = collision.gameObject.GetComponent<Renderer>().material;
                 Destroy(collision.gameObject);
                 PlayParticles();
                 break;
+            case 1:
+                Destroy(collision.gameObject);
+                PlayParticles();
+                Player.GetComponent<HealthScript>().health = health - 50;
+                break;
+            case 2:
+                Destroy(collision.gameObject);
+                break;
+            case 4:
+                Debug.log("It is a leaf");
+                break;
+            case 5:
+                Destroy(collision.gameObject);
+                Player.GetComponent<Inventory>().additem();
+                break;
+            case 6:
+                Destroy(collision.gameObject);
+                Debug.log("It's a fake!!!");
+                break;
+            case 8:
+                Collision.gameObject.GetComponent<HealthScript>().health = health - 50;
+                break;
+
 
             default:
                 break;
